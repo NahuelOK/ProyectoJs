0b2133ad1d03c170ab9fea35a03e88f17f531d75
nahuel moreno <nahuel23009@gmail.com>
2023-06-02 19:31:58 -0300

    v2

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-let boton = document.getElementById("botoncito");
+/* let boton = document.getElementById("botoncito");
 boton.addEventListener("click", megaFuntion);
 
 function megaFuntion() {
@@ -64,5 +64,73 @@ function megaFuntion() {
       parrafosA.textContent = estudiantesA[i];
       aprobados.appendChild(parrafosA);
     }
-}
+} */
 
+let nombreForm = "";
+let apellidoForm = "";
+let notaForm1 = 0;
+let notaForm2 = 0;
+let notaForm3 = 0;
+let notaForm4 = 0;
+let notaForm5 = 0; 
+let estudiantesA =[]
+let estudiantesD =[]
+let estudiante0;
+let aprobados = document.getElementById("aprobados");
+let desaprobados = document.getElementById("desaprobados");
+let parrafosA;
+let parrafosD;
+let suma=0;
+
+function enviarFormulario(event) {
+    event.preventDefault(); 
+  
+     nombreForm = document.getElementById("nombre-form").value
+     apellidoForm = document.getElementById("apellido-form").value
+     notaForm1 = parseFloat(document.getElementById("note1").value) 
+     notaForm2 = parseFloat(document.getElementById("note2").value) 
+     notaForm3 = parseFloat(document.getElementById("note3").value) 
+     notaForm4 = parseFloat(document.getElementById("note4").value) 
+     notaForm5 = parseFloat(document.getElementById("note5").value) 
+  
+    document.getElementById("formu").reset();
+    calculoPromedios()
+}
+class estudiante{
+    constructor(nombre, apellido, curso){
+        this.nombre = nombre;
+        this.apellido = apellido;
+        this.curso = curso;
+    }
+}
+function calculoPromedios(){
+    let notas = notaForm1 + "\n" +notaForm2 + "\n" + notaForm3 + "\n" + notaForm4 + "\n" + notaForm5 + "\n"
+    suma= notaForm1 + notaForm2+ notaForm3+ notaForm4+ notaForm5
+    let promedio = suma/5
+    estudiante0 = new estudiante (nombreForm,apellidoForm,"5to-B")
+
+    alert("Alumno: "+estudiante0.nombre +"\n"+"Las notas de "+estudiante0.nombre +" son: "+"\n"+notas+"Su promedio es de: "+promedio)
+
+    if (promedio >= 6){
+        alert("El alumno "+estudiante0.nombre+" aprobó la materia!")
+        estudiantesA.push(estudiante0.nombre +" "+ estudiante0.apellido +" "+"("+promedio+")")
+    }
+    else if (promedio < 6){
+        alert("El alumno "+estudiante0.nombre+" tiene que recursar la materia!")
+        estudiantesD.push(estudiante0.nombre +" "+ estudiante0.apellido +" "+"("+promedio+")")
+    }
+    else {
+        alert("Ingrese valores validos")
+    }
+    for (let i = 0; i < estudiantesD.length; i++) {
+        parrafosD = document.createElement("p");
+        parrafosD.textContent = estudiantesD[i];
+        desaprobados.appendChild(parrafosD);
+      }
+    for (let i = 0; i < estudiantesA.length; i++) {
+      parrafosA = document.createElement("p");
+      parrafosA.textContent = estudiantesA[i];
+      aprobados.appendChild(parrafosA);
+    }
+}
+    
