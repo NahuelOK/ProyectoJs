59ef9734cdd7b9d4d3ed4fb39b59209f0d9fcf3e
nahuel moreno <nahuel23009@gmail.com>
2023-06-02 12:48:00 -0300

    Agregue DOM

--- script.js
+++ script.js
@@ -1,71 +1,68 @@
-let estudiantesA = []
-let estudiantesD = []
-let nombre;
-let apagar;
-let elegir;
-let num;
-alert("Calcula los promedios de los estudiantes")
+let boton = document.getElementById("botoncito");
+boton.addEventListener("click", megaFuntion);
 
-class curso{
-    constructor(nombreC, cantidadEstudiantes, profesor){
-        this.nombreC = nombreC;
-        this.cantidadEstudiantes = cantidadEstudiantes;
-        this.profesor = profesor;
+function megaFuntion() {
+    class estudiante{
+        constructor(nombre, apellido, curso){
+            this.nombre = nombre;
+            this.apellido = apellido;
+            this.curso = curso;
+        }
+    }
+    
+    let estudiantesA =[]
+    let estudiantesD =[]
+    let estudiante0;
+    let apagar;
+    
+    function calculoPromedios(){
+        let nota;
+        let notas=""; 
+        let suma=0 
+        estudiante0 = new estudiante (prompt("Ingrese Nombre"),prompt("Ingrese Apellido"),"5to-B")
+        for (let i=1; i<=5; i++){
+            nota = parseInt(prompt("Ingrese nota")) 
+            notas += nota + "\n"
+            suma += nota 
+        }
+        let promedio = suma/5
+        alert("Alumno: "+estudiante0.nombre +"\n"+"Las notas de "+estudiante0.nombre +" son: "+"\n"+notas+"Su promedio es de: "+promedio)
+    
+        if (promedio >= 6){
+            alert("El alumno "+estudiante0.nombre+" aprobó la materia!")
+            estudiantesA.push(estudiante0.nombre +" "+ estudiante0.apellido +" "+"("+promedio+")")
+        }
+        else if (promedio < 6){
+            alert("El alumno "+estudiante0.nombre+" tiene que recursar la materia!")
+            estudiantesD.push(estudiante0.nombre +" "+ estudiante0.apellido +" "+"("+promedio+")")
+        }
+        else {
+            alert("Ingrese valores validos")
+        }
+        apagar = prompt("Para apagar el sistema Ingresar (ESC)" + "\n" + "De lo contrario pulse enter")
+        
+        estudiantesA.sort();
+        estudiantesD.sort();
+    }
+    
+    while(apagar != "ESC"){
+        calculoPromedios()
+    }
+    
+    let aprobados = document.getElementById("aprobados");
+    let desaprobados = document.getElementById("desaprobados");
+    let parrafosA;
+    let parrafosD;
+    
+    for (let i = 0; i < estudiantesD.length; i++) {
+        parrafosD = document.createElement("p");
+        parrafosD.textContent = estudiantesD[i];
+        desaprobados.appendChild(parrafosD);
+      }
+    for (let i = 0; i < estudiantesA.length; i++) {
+      parrafosA = document.createElement("p");
+      parrafosA.textContent = estudiantesA[i];
+      aprobados.appendChild(parrafosA);
     }
 }
 
-let curso1 = new curso (" 5to",20,"Profesor Jirafales")
-let curso2 = new curso (" 3ro",27,"Profesor Manuel")
-let curso3 = new curso (" 2do",16,"Profesor Theo")
-
-function elegirCurso(){
-    elegir = parseInt(prompt("Ingrese el curso" + "\n" + "1) para 5to"+ "\n" + "2) para 3ro"+ "\n" + "3) para 2do")) 
-    if(elegir == 1){
-        num = curso1.nombreC
-    }
-    else if(elegir == 2){
-        num = curso2.nombreC
-    }
-    else if(elegir == 3){
-        num = curso3.nombreC
-    }
-    else{
-        num = "    Curso: Desconocido"
-    }
-}
-
-function calculoPromedios(){
-    nombre = prompt("Ingrese el nombre del alumno") 
-    elegirCurso()
-    let nota;
-    let notas=""; 
-    let suma=0 
-    for (let i=1; i<=5; i++){
-        nota = parseInt(prompt("Ingrese nota")) 
-        notas += nota + "\n"
-        suma += nota 
-    }
-    let promedio = suma/5
-    alert("Alumno: "+nombre + num +"\n"+"Las notas de "+nombre +" son: "+"\n"+notas+"Su promedio es de: "+promedio)
-
-    if (promedio >= 6){
-        alert("El alumno "+nombre+" aprobó la materia!")
-        estudiantesA.push(nombre)
-    }
-    else if (promedio < 6){
-        alert("El alumno "+nombre+" tiene que recursar la materia!")
-        estudiantesD.push(nombre)
-    }
-    else {
-        alert("Ingrese valores validos")
-    }
-    apagar = prompt("Para apagar el sistema Ingresar (ESC)" + "\n" + "De lo contrario pulse enter")
-    estudiantesA.sort();
-    estudiantesD.sort();
-}
-
-while(apagar != "ESC"){
-    calculoPromedios()
-}
-
-alert("Aprobados: "+estudiantesA +"\n"+ "\n" + "Desaprobados: "+estudiantesD)
